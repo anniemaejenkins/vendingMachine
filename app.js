8d6c0a081a54e532a567392285114603ed1074a5
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-07-11 15:01:16 -0400

    added mongo

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // const bodyParser = require('body-parser');
 // mongoose.Promise = require('bluebird');
 
 const app = express();
 
-// const nodeEnv = process.env.NODE_ENV || "development";
-// const config = require("./config.json")[nodeEnv];
+const nodeEnv = process.env.NODE_ENV || "development";
+const config = require("./config.json")[nodeEnv];
 
+mongoose.connect(config.mongoURL);
 
 app.get("/api/sanity", (req, res) => {
   res.json({hello: "annie"});
